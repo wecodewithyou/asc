252c0ace8c5de89d98e506a4f9371ea77c38f6eb
wecodewithyou <wecodewithyou2020@protonmail.com>
2020-07-31 21:25:18 +0530

    Added Chemistry Entries

--- menus.cpp
+++ menus.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
 #include "physics.h"
+#include "chemistry.h"
 using namespace std;
 
 class sub_menu{ // Menu to show chapters of each subject
     public:
         void sub_menu_physics();
+        void sub_menu_chemistry();
         /** Upcoming sub_menus
           * void sub_menu_maths();
-          * void sub_menu_physics();
           * void sub_menu_csc();
         **/
         class problems_menu{ // Class to hold problems database from chapters
             public:
                 void physics(int chapter); // with chapter number arguement
-                
+                void chemistry(int chapter);
                 /** Upcoming problems_menu
                   * void maths();
-                  * void chemistry();
                   * void csc();
                 **/
         };
 };
 
+sub_menu::problems_menu pb; // Intializing variable to the subclass
+
 void sub_menu::sub_menu_physics() // Function inside a class
 {
-    sub_menu::problems_menu pb; // Intializing variable to the subclass
     int op;
     cout << " 1. Electric Charges and Fields " << endl;
     cout << " Enter a Option : ";
@@ -36,6 +37,22 @@ void sub_menu::sub_menu_physics() // Function inside a class
             break;
         default:
             cout << " Invalid Option Please Try Again " << endl;
+    }
+}
+
+void sub_menu::sub_menu_chemistry()
+{
+    int op;
+    cout << " 1. Solid States " << endl;
+    cout << " Enter a Option : ";
+    cin >> op;
+    switch(op)
+    {
+        case 1:
+            pb.chemistry(1);
+            break;
+        default:
+            cout << " Invalid Option Please Try Again " << endl; 
     }
 }
 
@@ -64,6 +81,21 @@ void sub_menu::problems_menu::physics( int chapter) // Initializing function ins
             cout << Edipole() << endl;
         else if(o == 6)
             torque();
+        else
+            cout << " Invalid Option " << endl;
+    }
+}
+
+void sub_menu::problems_menu::chemistry(int chapter)
+{
+    if(chapter == 1)
+    {
+        int o;
+        cout << " 1. Density of the unit Cell " << endl;
+        cout << " Enter a Option : ";
+        cin >> o;
+        if(o == 1)
+            cout << density() << endl;
         else
             cout << " Invalid Option " << endl;
     }
