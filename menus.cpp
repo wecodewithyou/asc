e7307771f99937ab5b9a605343125b7b823e91b4
wecodewithyou <wecodewithyou2020@protonmail.com>
2020-07-31 18:24:50 +0530

    Reorganized Menus

--- menus.cpp
+++ menus.cpp
@@ -26,7 +26,7 @@ void sub_menu::sub_menu_physics() // Function inside a class
 {
     sub_menu::problems_menu pb; // Intializing variable to the subclass
     int op;
-    cout << " 1. Chapter 1 " << endl;
+    cout << " 1. Electric Charges and Fields " << endl;
     cout << " Enter a Option : ";
     cin >> op;
     switch(op)
@@ -44,20 +44,26 @@ void sub_menu::problems_menu::physics( int chapter) // Initializing function ins
     if(chapter == 1)
     {
         int o;
-        cout << " 1. Find Q " << endl;
+        cout << " 1. Find Quantization " << endl;
         cout << " 2. Find Force Between 2 charges " << endl;
         cout << " 3. Find Electric Field " << endl;
         cout << " 4. Find Electric Flux " << endl;
+        cout << " 5. Find Electric Dipole " << endl;
+        cout << " 6. Find Torque " << endl;
         cout << " Enter a Option : ";
         cin >> o;
         if(o == 1)
             q(); // calling function inside the headerfile
         else if(o == 2)
-            f();
+            cout << f() << endl;
         else if(o == 3)
-            E();
+            cout << E() << endl;
         else if(o == 4)
-            Eflux();
+            cout << Eflux() << endl;
+        else if(o == 5)
+            cout << Edipole() << endl;
+        else if(o == 6)
+            torque();
         else
             cout << " Invalid Option " << endl;
     }
