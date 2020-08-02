a74d9bdce47f048a55dc23eb048b90582c1a5048
wecodewithyou <wecodewithyou2020@protonmail.com>
2020-08-02 14:28:20 +0530

    Reorganized Menu

--- menus.cpp
+++ menus.cpp
@@ -15,16 +15,58 @@ class sub_menu{ // Menu to show chapters of each subject
         };
 };
 
+sub_menu sb;
 sub_menu::problems_menu pb; // Intializing variable to the subclass
 
-void sub_menu::sub_menu_physics() // Function inside a class
+void main_menu()
 {
-    int op;
-    cout << " 1. Electric Charges and Fields " << endl;
+    int op; 
+    cout << "\n MAIN MENU:- " << endl;
+    cout << " 1. Physics " << endl;
+    cout << " 2. Chemistry " << endl;
+    cout << " 4. About the Program " << endl;
+    cout << " 0. Exit the Program " << endl;
     cout << " Enter a Option : ";
     cin >> op;
     switch(op)
     {
+        case 1:
+            sb.sub_menu_physics(); // calling function inside the class
+            break;
+        case 2:
+            sb.sub_menu_chemistry();
+            break;
+        case 4:
+            cout << "\n\t\t\t\t\t ABOUT THE PROGRAM \n\n";
+            cout << " Build   : Pre-Alpha Build "  << endl;
+            cout << " Version : v0.1 pre-alpha build " << endl;
+            cout << "\n Description : A simple all in one science subject"<<endl<<" problem solver for +2 Students " << endl;
+            cout << "               Developed by Spectr3 , Ph0en1x and Mr.Unkn0wn " << endl;
+            cout << "\n Thank You For using our program <3 <3 <3 \n\n" << endl;
+            break;
+        case 0:
+            cout << "\n Closing Program ...... " << endl;
+            exit(0);
+            break;
+        default:
+            cout << " Invalid Option Please Try Again " << endl;
+            exit(1);
+    } 
+}
+
+void sub_menu::sub_menu_physics() // Function inside a class
+{
+    int op;
+    cout << "\n CHAPTER MENU:- " << endl;
+    cout << " 1. Electric Charges and Fields " << endl;
+    cout << " 0. Back to Main Menu " << endl;
+    cout << " Enter a Option : ";
+    cin >> op;
+    switch(op)
+    {
+	case 0:
+	    main_menu();
+	    break;
         case 1:
             pb.physics(1);
             break;
@@ -36,11 +78,16 @@ void sub_menu::sub_menu_physics() // Function inside a class
 void sub_menu::sub_menu_chemistry()
 {
     int op;
+    cout << "\n CHAPTER MENU:- " << endl;
     cout << " 1. Solid States " << endl;
+    cout << " 0. Back to Main Menu " << endl;
     cout << " Enter a Option : ";
     cin >> op;
     switch(op)
     {
+	case 0:
+	   main_menu();
+	   break;
         case 1:
             pb.chemistry(1);
             break;
@@ -51,6 +98,7 @@ void sub_menu::sub_menu_chemistry()
 
 void sub_menu::problems_menu::physics( int chapter) // Initializing function inside a subclass
 {
+    cout << "\n PROBLEMS MENU:- " << endl;
     if(chapter == 1)
     {
         int o;
@@ -62,9 +110,12 @@ void sub_menu::problems_menu::physics( int chapter) // Initializing function ins
         cout << " 6. Find Torque " << endl;
         cout << " 7. EField of EDipole at Axiel Line " << endl;
         cout << " 8. EField of EDipole at Equatorial Line " << endl;
-        cout << " Enter a Option : ";
+        cout << " 0. Back to Chapter Menu " << endl;
+	cout << " Enter a Option : ";
         cin >> o;
-        if(o == 1)
+	if(o == 0)
+	    sb.sub_menu_physics();
+	else if(o == 1)
             q(); // Quantization
         else if(o == 2)
             cout << f() << endl; // Force
@@ -87,13 +138,17 @@ void sub_menu::problems_menu::physics( int chapter) // Initializing function ins
 
 void sub_menu::problems_menu::chemistry(int chapter)
 {
+    cout << "\n PROBLEMS MENU:- " << endl;
     if(chapter == 1)
     {
         int o;
         cout << " 1. Density of the unit cell " << endl;
-        cout << " Enter a Option : ";
+        cout << " 0. Back to Chapter Menu " << endl;
+	cout << " Enter a Option : ";
         cin >> o;
-        if(o == 1)
+	if(o == 0)
+	    sb.sub_menu_chemistry();
+	else if(o == 1)
             cout << density() << endl; // Density of the unit cell
         else
             cout << " Invalid Option " << endl;
