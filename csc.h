fb89fc224950247fdfc8fe64186c816fce8a6d2c
wecodewithyou <wecodewithyou2020@protonmail.com>
2020-08-04 21:12:34 +0530

    Fixed Errors

--- csc.h
+++ csc.h
@@ -1,3 +1,6 @@
+#ifndef __CSC_H_INCLUDED__
+#define __CSC_H_INCLUDED__
+
 #include<iostream>
 using namespace std;
 
@@ -12,27 +15,30 @@ void marklist( )
 {
    student s[50];
    int n,i,tot_score[50];
-   cout<<”Enter the number of students :”;
-   cin>>n;
+   cout << "\n Marklist:- " << endl;
+   cout << " Enter the number of students : ";
+   cin >> n;
    for(i=0;i<n;i++)
    {
-      cout<<”Enter the register number :”;
-      cin>>s[i].reg_no;
-      cout<<”Enter the name :”;
+      cout <<" Enter the register number : ";
+      cin >> s[i].reg_no;
+      cin.ignore();
+      cout <<" Enter the name : ";
       cin.getline(s[i].name,20);
-      cout<<”Enter the scores in C.E,P.E and T.E. :”
-      cin>>s[i].ce>>s[i].pe>>s[i].te;
-      tot_score[i]=s[i].ce+s[i].pe+s[i].te;
+      cout <<" Enter the scores in C.E,P.E and T.E. : ";
+      cin >> s[i].ce >> s[i].pe >> s[i].te;
+      tot_score[i] = s[i].ce + s[i].pe + s[i].te;
    }
-   cout<<”The student details are......\n”;
+   cout<<" The student details are......\n";
    for(i=0;i<n;i++)
    {
-      cout<<”Register Number :”<<s[i].regno<<”\n”;
-      cout<<”Name :”<<s[i].name<<”\n”;
-      cout<<”C.E. score :”<<s[i].ce<<”\n”;
-      cout<<”P.E. score :”<<s[i].pe<<”\n”;
-      cout<<”T.E. score :”<<s[i].te<<”\n”;
-      cout<<”Total score :”<<tot_score[i];
+      cout<< " Register Number : " <<s[i].reg_no<<endl;
+      cout<< " Name : " <<s[i].name<<endl;
+      cout<< " C.E. score : "<<s[i].ce <<endl;
+      cout<< " P.E. score : "<<s[i].pe<<endl;
+      cout<< " T.E. score : "<<s[i].te<<endl;
+      cout<< " Total score : "<<tot_score[i];
    }
-   return 0;
 }
+
+#endif
